1baac36c8b788ca62eebe097e918d98777f26d97
Emmet O Neill <emmetoneill18@gmail.com>
2025-08-15 11:53:40 +0100

    Build admin panel with user management (CRUD)

--- view/header_admin.php
+++ view/header_admin.php
@@ -1,47 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-      <title>The Golf Shop.</title>
-      <link rel="stylesheet" href="<?php echo $app_path ?>style.css" />
-      <link rel="stylesheet" href="<?php echo $app_path ?>css/bootstrap.css" />
-      <script src="<?php echo $app_path ?>jquery-3.3.1.min.js"></script>
-    <!-- For Pagination in Product_List.php -->
-      <script>
-        $(document).ready(function() {
-          $("#num_rows").change(function() {
-              $("#pagination_form").submit();  // A form in Product_List.php
-          });
-        });
-      </script>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    </head>
-   <body>
-      <?php
-      // Check if administrator is logged in and display login status
-        $account_url = $app_path . 'admin/users/';
-        $logout_url = $account_url . '?action=logout';
-        if (isset($_SESSION['admin'])) :
-      ?>
-      <div class="row mr-0 mt-n2 mb-n2">
-        <div class="col-md-12">
-          <div id="headlinks">
-            <p>
-              <span class="fa fa-user"></span><b><?php echo ' Hi, ' . $_SESSION['admin'][1].'!'; ?></b> &nbsp&nbsp<a href="<?php echo $logout_url; ?>"><span class="fa fa-sign-out"></span>Logout</a>
-            </p>
-          </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Panel - TeeTime Golf Shop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo $app_path ?>public/css/All_Styles.css" />
+</head>
+<body>
+
+<?php
+    $dashboard_url = $app_path . 'admin/';
+    $products_url = $app_path . 'admin/product/';
+    $orders_url = $app_path . 'admin/orders/';
+    $categories_url = $app_path . 'admin/category/';
+    $account_url = $app_path . 'admin/users/';
+    $logout_url = $account_url . '?action=logout';
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo $dashboard_url; ?>">TeeTime Admin</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#adminNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="adminNavbar">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="<?php echo $products_url; ?>">Products</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $orders_url; ?>">Orders</a></li>
+                <li class="nav-item"><a class="nav-link" href="<?php echo $categories_url; ?>">Categories</a></li>
+            </ul>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <?php if (isset($_SESSION['admin'])) : ?>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            <span class="fa fa-user"></span> Hi, <?php echo htmlspecialchars($_SESSION['admin']['fName']); ?>!
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li><a class="dropdown-item" href="<?php echo $account_url; ?>">Manage Accounts</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="<?php echo $logout_url; ?>"><span class="fa fa-sign-out"></span> Logout</a></li>
+                        </ul>
+                    </li>
+                <?php else: ?>
+                    <li class="nav-item"><a class="nav-link" href="<?php echo $account_url; ?>"><span class="fa fa-sign-in"></span> Login</a></li>
+                <?php endif; ?>
+            </ul>
         </div>
-      </div>
-      <?php else: ?>
-        <div class="searchbar">
-          <div class="row mr-0 mt-n2 mb-n2">  
-            <div class="col-md-12">
-              <div id="headlinks">
-                <p><a href="<?php echo $account_url; ?>"><span class="fa fa-sign-in"></span> Login</a></p>
-              </div>
-            </div>
-          </div>
-        </div>
-      <?php endif; ?>
-      <?php require_once('navbar_admin.php'); ?>
+    </div>
+</nav>
+
+<main class="container mt-4">
